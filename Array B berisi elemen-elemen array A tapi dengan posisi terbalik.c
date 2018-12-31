bdfd1e9d261f53d537a0978c64f87dc15db59abe
Jajang Jaelani <45264588+Jajang1293@users.noreply.github.com>
2018-12-31 21:53:43 +0700

    Add files via upload

---
+++ Array B berisi elemen-elemen array A tapi dengan posisi terbalik.c
@@ -0,0 +1,23 @@
+#include<conio.h>
+#include<stdio.h>
+
+int main(void)
+{
+    int c[5]={5,10,6,0,4};
+    int d[5];
+    int i=0, j=0;
+    printf ("Nilai array yang akan ditampilkan\n");
+    for(i=4; i>=0; i--)
+        {
+        d[i]=c[j];
+        printf ("%d", c[j]);
+        j++;}
+
+    printf ("\n");
+    printf ("Posisi terbalik dari array A ke array B sebagai berikut\n");
+    for (i=0; i<5; i++){
+        printf("%d", d[i]);
+    }
+    printf("\n");
+    return 0;
+}
